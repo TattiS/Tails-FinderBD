b3005c4b5f95fcc0ba33c0324f28315f9016e0be
Tetyana Solyanyk <tanyas81@gmail.com>
2025-09-05 09:24:10 +0300

    feat: add jest for testing and update package.json scripts
    fix: enhance reverseGeocode function to accept Ukrainian language in API request

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const reverseGeocode = async (lat, lng) => {
-  const url = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lng}`;
+  const url = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lng}&accept-language=uk`;
+
   try {
     const response = await axios.get(url, {
       headers: { 'User-Agent': 'tailsfinder-app' },
